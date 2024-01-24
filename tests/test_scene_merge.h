fe3100a534087665ddd1eabc11745e5e8ac76bc3
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-01-24 14:00:28 -0800

    Add a test.

--- tests/test_scene_merge.h
+++ tests/test_scene_merge.h
@@ -4,11 +4,31 @@
 
 #include "tests/test_macros.h"
 
+#include "modules/scene_merge/mesh_merge_triangle.h"
 namespace TestSceneMerge {
 
 TEST_CASE("[Modules][SceneMerge] SceneMerge instantiates") {
 	String hello = "SceneMerge instantiates.";
 	CHECK(hello == "SceneMerge instantiates.");
+}
+
+bool mock_callback(void *param, int x, int y, const Vector3 &bar, const Vector3 &dx, const Vector3 &dy, float coverage) {
+    CHECK(x == 1);
+    CHECK(y == 2);
+    CHECK(bar == Vector3(3, 4, 5));
+    CHECK(dx == Vector3(6, 7, 8));
+    CHECK(dy == Vector3(9, 10, 11));
+    CHECK(coverage == 0.5f);
+
+    return true;
+}
+
+TEST_CASE("[Modules][SceneMerge] MeshMergeTriangle drawAA") {
+    Vector2 v0(1, 2), v1(3, 4), v2(5, 6);
+    Vector3 t0(7, 8, 9), t1(10, 11, 12), t2(13, 14, 15);
+    MeshMergeTriangle triangle(v0, v1, v2, t0, t1, t2);
+
+    CHECK(triangle.drawAA(mock_callback, nullptr));
 }
 
 } // namespace TestSceneMerge
